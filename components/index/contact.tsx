bafeca954ef44b4d968e397524cc50f222a9d031
jagnani73 <yashjagnani73@gmail.com>
2021-07-11 04:49:00 +0530

    🚑 wip contact

--- components/index/contact.tsx
+++ components/index/contact.tsx
@@ -2,7 +2,7 @@ export const Contact = () => {
   return (
     <div
       className="calendly-inline-widget"
-      dataUrl="https://calendly.com/jagnani73?hide_landing_page_details=1&hide_gdpr_banner=1"
+      // dataUrl="https://calendly.com/jagnani73?hide_landing_page_details=1&hide_gdpr_banner=1"
       style={{ minWidth: "320px", height: "630px" }}
     />
   );
