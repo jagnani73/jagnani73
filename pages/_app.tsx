03c107d7763f24500da63be0a03158d920dddcbb
jagnani73 <yashjagnani73@gmail.com>
2021-07-11 04:35:07 +0530

    ♻️ head imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,9 @@
-import Head from "next/head";
 import { AppProps } from "next/app";
 
 import "../styles/tailwind.styles.css";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Head>
-        <script
-          type="text/javascript"
-          src="https://assets.calendly.com/assets/external/widget.js"
-          async
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
+import Head from "next/head";
+
 import * as IndexComponents from "../components/index";
 
 const IndexPage = () => {
   return (
     <>
+      <Head>
+        <script
+          type="text/javascript"
+          src="https://assets.calendly.com/assets/external/widget.js"
+          async
+        />
+      </Head>
+
       <IndexComponents.Home />
       <IndexComponents.About />
       <IndexComponents.Stack />
