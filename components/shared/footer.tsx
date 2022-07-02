9cf4b634f1df364a0710a23d4c487a0a85745681
jagnani73 <yashjagnani73@gmail.com>
2022-07-02 20:05:30 +0530

    wip: footer

--- components/shared/footer.tsx
+++ components/shared/footer.tsx
@@ -1,19 +1,17 @@
 import Link from "next/link";
 
+import { SocialIcon } from ".";
 import {
   FOOTER_ROUTES,
-  ROUTES,
-  SOCIALS_NAMES,
   SOCIALS_ROUTES,
 } from "../../utils/constants/shared-constants";
-import SocialIcon from "./social-icon";
 
 const Footer: React.FC = () => {
   return (
     <footer className="section-container mb-16">
-      <div className="flex p-12">
+      <div className="flex flex-col lg:flex-row lg:p-12">
         {FOOTER_ROUTES.map(({ title, routes }) => (
-          <div className="flex flex-col gap-y-2 w-full">
+          <div key={title} className="flex flex-col gap-y-2 w-full mb-8">
             <h5 className="font-semibold mb-4">{title}</h5>
             {routes.map(({ name, href, external }) => (
               <Link key={name + href} href={href}>
@@ -30,16 +28,16 @@ const Footer: React.FC = () => {
         ))}
 
         <div className="w-full flex flex-col">
-          <div className="flex items-center justify-end">
-            <h3>Yashvardhan Jagnani</h3>
-            <figure className="w-16 h-16 rounded-full ml-8 bg-white"></figure>
+          <div className="flex flex-col-reverse lg:flex-row lg:items-center justify-end">
+            <h3 className="font-bold mt-4 lg:mt-0">Yashvardhan Jagnani</h3>
+            <figure className="w-16 h-16 rounded-full lg:ml-8 bg-white"></figure>
           </div>
 
-          <div className="flex gap-4 mt-auto pt-2">
+          <div className="flex gap-4 mt-auto pt-8 lg:pt-2 justify-between max-w-xs lg:max-w-none">
             {SOCIALS_ROUTES.map(({ href, external, icon }) => (
               <Link key={href} href={href}>
                 <a
-                  className="uppercase text-sm"
+                  className="uppercase text-sm w-6 lg:w-8"
                   rel="noopener noreferrer"
                   target={external ? "_blank" : ""}
                 >
@@ -51,7 +49,7 @@ const Footer: React.FC = () => {
         </div>
       </div>
 
-      <p className="text-center font-medium mt-8">
+      <p className="lg:text-center font-medium mt-8">
         Sit commodo est exercitation culpa aliqua pariatur.
       </p>
     </footer>
--- components/shared/index.tsx
+++ components/shared/index.tsx
@@ -3,5 +3,6 @@ export { default as Project } from "./project";
 export { default as Navbar } from "./navbar";
 export { default as Footer } from "./footer";
 export { default as StackIcon } from "./stack-icon";
+export { default as SocialIcon } from "./social-icon";
 export { default as LinkIcon } from "./link-icon";
 export { default as Contact } from "./contact";
