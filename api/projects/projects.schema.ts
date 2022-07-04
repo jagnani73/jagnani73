bc37a09a01534e0ec4c0a5cee9995bda390f6a60
jagnani73 <yashjagnani73@gmail.com>
2022-07-05 00:54:31 +0530

    fix(api/projects): schema

--- api/projects/projects.schema.ts
+++ api/projects/projects.schema.ts
@@ -3,6 +3,7 @@ import * as yup from "yup";
 export const ProjectSchema = yup
   .object({
     preview: yup.string().trim().required().url(),
+    images: yup.array().of(yup.string().trim().url()),
     name: yup.string().trim().required(),
     description: yup.string().trim().required(),
     tag: yup.string().trim().required(),
