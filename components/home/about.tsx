b3fab303d0acdd5f0408129225aff0f570cbdcac
jagnani73 <yashjagnani73@gmail.com>
2022-07-05 04:10:05 +0530

    fix(clien/home): about

--- components/home/about.tsx
+++ components/home/about.tsx
@@ -21,10 +21,10 @@ const About: React.FC = () => {
           src="https://spotify-recently-played-readme.vercel.app/api?user=6o2i4ysx7nesg69llfcreqqt5&unique=true"
         />
 
-        {/* <iframe
+        <iframe
           className="mt-10 md:mt-0 md:w-8/12 h-96"
           src="https://stellarium-web.org/"
-        /> */}
+        />
       </div>
     </section>
   );
