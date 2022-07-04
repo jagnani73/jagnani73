1f3ef6a81e24432a8e082c0750a04f966d4fdddd
jagnani73 <yashjagnani73@gmail.com>
2022-07-05 04:09:11 +0530

    feat: add meta & og

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -30,6 +30,49 @@ class MyDocument extends Document {
           />
           <meta name="msapplication-TileColor" content="#232323" />
           <meta name="theme-color" content="#232323" />
+
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+          <meta charSet="utf-8" />
+          <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+          <meta httpEquiv="content-language" content="en" />
+          <meta name="language" content="English" />
+
+          <meta name="author" content="Yashvardhan Jagnani" />
+          <meta
+            name="description"
+            content=" He is a Computer Science Engineering student graduating from the class of 2023 from SRMIST, Chennai, India. Doing a specialization in Cybersecurity, his interest peeked in software development back in his fresher year of college. Ever since, he has been passionate about ideating solutions using software development. He inclines towards Frontend Development, especially ReactJS. Yashvardhan has laid the foundation for many communities and projects. He strives for a pursuit that reciprocates in-depth knowledge for his diligent work towards adding value to his professional as well as personal aspects—an amalgamation of interpersonal and technical skill sets with a belief in continuous learning."
+          />
+          <meta name="image" content="/site/og-image.png" />
+
+          <meta itemProp="name" content="Yashvardhan Jagnani" />
+          <meta
+            itemProp="description"
+            content=" He is a Computer Science Engineering student graduating from the class of 2023 from SRMIST, Chennai, India. Doing a specialization in Cybersecurity, his interest peeked in software development back in his fresher year of college. Ever since, he has been passionate about ideating solutions using software development. He inclines towards Frontend Development, especially ReactJS. Yashvardhan has laid the foundation for many communities and projects. He strives for a pursuit that reciprocates in-depth knowledge for his diligent work towards adding value to his professional as well as personal aspects—an amalgamation of interpersonal and technical skill sets with a belief in continuous learning."
+          />
+          <meta itemProp="image" content="/site/og-image.png" />
+
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:title" content="Yashvardhan Jagnani" />
+          <meta
+            name="twitter:description"
+            content=" He is a Computer Science Engineering student graduating from the class of 2023 from SRMIST, Chennai, India. Doing a specialization in Cybersecurity, his interest peeked in software development back in his fresher year of college. Ever since, he has been passionate about ideating solutions using software development. He inclines towards Frontend Development, especially ReactJS. Yashvardhan has laid the foundation for many communities and projects. He strives for a pursuit that reciprocates in-depth knowledge for his diligent work towards adding value to his professional as well as personal aspects—an amalgamation of interpersonal and technical skill sets with a belief in continuous learning."
+          />
+          <meta name="twitter:site" content="@jagnani73" />
+          <meta name="twitter:creator" content="@jagnani73" />
+          <meta name="twitter:image:src" content="/site/og-image.png" />
+
+          <meta name="og:title" content="Yashvardhan Jagnani" />
+          <meta
+            name="og:description"
+            content=" He is a Computer Science Engineering student graduating from the class of 2023 from SRMIST, Chennai, India. Doing a specialization in Cybersecurity, his interest peeked in software development back in his fresher year of college. Ever since, he has been passionate about ideating solutions using software development. He inclines towards Frontend Development, especially ReactJS. Yashvardhan has laid the foundation for many communities and projects. He strives for a pursuit that reciprocates in-depth knowledge for his diligent work towards adding value to his professional as well as personal aspects—an amalgamation of interpersonal and technical skill sets with a belief in continuous learning."
+          />
+          <meta name="og:image" content="/site/og-image.png" />
+          <meta name="og:url" content="https://jagnani73.com" />
+          <meta name="og:site_name" content="Yashvardhan Jagnani" />
+          <meta name="og:type" content="website" />
         </Head>
 
         <body>
