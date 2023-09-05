ce3d1161be46d0194a1f779cef66fd7ae69b0012
jagnani73 <yashjagnani73@gmail.com>
2023-09-05 19:27:16 +0530

    !fix(home): github calendar

--- website/components/home/hero.tsx
+++ website/components/home/hero.tsx
@@ -1,53 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { useEffect } from "react";
-import GitHubCalendar from "github-calendar";
-import "github-calendar/dist/github-calendar-responsive.css";
+import { cloneElement } from "react";
+import { Tooltip } from "react-tooltip";
+import GitHubCalendar from "react-github-calendar";
 
 const Hero: React.FC = () => {
-  useEffect(() => {
-    (async () => {
-      await GitHubCalendar("#github_contributions_graph", "jagnani73", {
-        global_stats: false,
-        summary_text: "",
-        tooltips: true,
-      });
-      var cssVariables = document.querySelector(":root");
-
-      (cssVariables as HTMLElement).style.setProperty(
-        "--color-calendar-graph-day-bg",
-        "#323232"
-      );
-      (cssVariables as HTMLElement).style.setProperty(
-        "--color-calendar-graph-day-L1-bg",
-        "#2d925c"
-      );
-      (cssVariables as HTMLElement).style.setProperty(
-        "--color-calendar-graph-day-L2-bg",
-        "#1b6b2c"
-      );
-      (cssVariables as HTMLElement).style.setProperty(
-        "--color-calendar-graph-day-L3-bg",
-        "#3ab34e"
-      );
-      (cssVariables as HTMLElement).style.setProperty(
-        "--color-calendar-graph-day-L4-bg",
-        "#2bc945"
-      );
-
-      const _ = document.getElementsByClassName(
-        "width-full f6 px-0 px-md-5 py-1"
-      );
-      for (var i = 0; i < _.length; i++) _[i].classList.add("hidden");
-
-      const graphSVG = document.querySelector(
-        ".js-calendar-graph"
-      ) as HTMLElement;
-      graphSVG.style.overflow = "auto";
-      graphSVG.style.paddingBottom = "0.5rem";
-    })();
-  }, []);
-
   return (
     <section className="lg:h-screen flex flex-wrap text-center">
       <div className="w-full lg:w-1/2 flex-center flex-col">
@@ -73,10 +30,10 @@ const Hero: React.FC = () => {
         </div>
       </div>
 
-      <div className="w-full lg:w-1/2 lg:bg-steel-blue flex-center flex-col lg:pt-40">
+      <div className="w-full lg:w-1/2 lg:bg-steel-blue flex-center flex-col lg:pt-20">
         <p className="w-10/12 lg:w-9/12 mx-auto">
-          He is a Computer Science Engineering student graduating from the class
-          of 2023 from SRMIST, Chennai, India. Doing a specialization in
+          He is a Computer Science Engineering student who graduated from the
+          class of 2023 from SRMIST, Chennai, India. Doing a specialization in
           Cybersecurity, his interest peeked in software development back in his
           fresher year of college. Ever since, he has been passionate about
           ideating solutions using software development. He inclines towards
@@ -88,11 +45,21 @@ const Hero: React.FC = () => {
           in continuous learning.
         </p>
 
-        <div className="mx-auto w-11/12 mt-8">
-          <div
-            id="github_contributions_graph"
-            className="mx-auto w-fit max-w-full"
-          />
+        <div className="mx-auto w-11/12 mt-8 overflow-x-auto">
+          <div className="mx-auto min-w-[844px]">
+            <Tooltip id="react-tooltip" />
+            <GitHubCalendar
+              username="jagnani73"
+              colorScheme="dark"
+              fontSize={12}
+              renderBlock={(block, activity) =>
+                cloneElement(block, {
+                  "data-tooltip-id": "react-tooltip",
+                  "data-tooltip-html": `${activity.count} activities on ${activity.date}`,
+                })
+              }
+            />
+          </div>
         </div>
       </div>
     </section>
